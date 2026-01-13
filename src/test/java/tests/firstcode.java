b1f0a12796561a0c4490364df2392928d8ee54ce
Divya Chaudhary <divya099chaudhary@gmail.com>
2026-01-14 01:18:52 +0530

    first push

---
+++ src/test/java/tests/firstcode.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class firstcode {
+    @Test
+    public void openBrowser() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
